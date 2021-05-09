e7ecce63dc3f6024ce714c7f713b2819d7c3dfb1
B-Ozturk <302938654@student.rocmondriaan.nl>
2021-05-09 19:23:49 +0200

    Version 6.3

--- BKO/js/main.js
+++ BKO/js/main.js
@@ -24,9 +24,6 @@ const changeHandler = function() {
     const introductie = document.querySelector(".intro");
     introductie.classList.toggle("introLM");
 
-    const demo = document.getElementById("demo");
-    demo.classList.toggle("demoLM");
-
     let blok, b;
     blok = document.querySelectorAll(".blok");
     for (b = 0; b < blok.length; b++) {
@@ -68,33 +65,3 @@ setInterval(function(){
     }
 
 }, 1000);
-
-function showTime(){
-    const date = new Date();
-    let h = date.getHours(); // 0 - 23
-    let m = date.getMinutes(); // 0 - 59
-    let s = date.getSeconds(); // 0 - 59
-    let session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    let time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    
-    setTimeout(showTime, 1000);
-    
-}
-
-showTime();
